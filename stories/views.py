31d853244b127b6f2d0f25c117be1bc0b2961e81
Punit Jain <contactpunitjain@gmail.com>
2020-06-07 14:55:07 +0530

    Added detail view and url for story

--- stories/views.py
+++ stories/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Story
 
 
@@ -6,3 +6,15 @@ def get_all_stories(request):
     allstories = Story.objects.all()
     return render(request, 'stories/list_stories.html',
                   {'allstories': allstories})
+
+
+def story_detail(request, year, month, day, story):
+    story = get_object_or_404(Story, slug=story,
+                              status='draft',
+                              publish__year=year,
+                              publish__month=month,
+                              publish__day=day)
+    print(story)
+    return render(request,
+                  'stories/detail_story.html',
+                  {'story': story})

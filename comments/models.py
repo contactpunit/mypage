32878ff0ca9beb52914958dde1fe10b013b9e4e8
Punit Jain <contactpunitjain@gmail.com>
2020-06-07 16:31:05 +0530

    added model and forms for comments app

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'comment from {self.email} on {self.created}'

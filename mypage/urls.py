ca794c3c24703ec2924eecaa05ae7e4f45b5c17b
Punit Jain <contactpunitjain@gmail.com>
2020-06-07 23:04:59 +0530

    Added login page view and url and template

--- mypage/urls.py
+++ mypage/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stories/', include('stories.urls', namespace='stories'))
+    path('stories/', include('stories.urls', namespace='stories')),
+    path('account/', include('account.urls', namespace='account')),
 ]

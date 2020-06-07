7c5db85afa04d7d209d1ec608fe23ad9a0131481
Punit Jain <contactpunitjain@gmail.com>
2020-06-07 09:22:57 +0530

    added new project myapp and app myapp

---
+++ stories/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Story(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+    title = models.CharField(max_length=120)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stories_story')
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default='draft')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-publish')
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'

b0ee894c5c918f47328139f9fbbba8576816d5e0
Punit Jain <contactpunitjain@gmail.com>
2020-06-10 14:45:42 +0530

    added registration form for new users and linked to login page

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,24 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+    def clean_password2(self):
+        cleandata = self.cleaned_data
+        if cleandata['password'] != cleandata['password2']:
+            raise forms.ValidationError("Passwords don't match. Kindly retype")
+        return cleandata['password']
--- account/views.py
+++ account/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -10,3 +9,26 @@ def dashboard(request):
     return render(request,
                   'registration/dashboard.html',
                   {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request,
+                          'registration/register_done.html',
+                          {'new_user': new_user})
+        else:
+            return render(request,
+                          'registration/register.html',
+                          {'user_form': user_form})
+
+    else:
+        user_form = UserRegistrationForm()
+        return render(request,
+                      'registration/register.html',
+                      {'user_form': user_form})

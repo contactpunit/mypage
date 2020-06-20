390fe604f9dfec492481d439971d4e4ae0c85157
Punit Jain <contactpunitjain@gmail.com>
2020-06-20 17:16:49 +0530

    added newstoey view

--- stories/views.py
+++ stories/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Story
 from comments.forms import CommentForm
+from stories.forms import StoryForm
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
+def add_story(request):
+    if request.method == 'POST':
+        form = StoryForm(request.POST)
+        if form.is_valid():
+            pending_form = form.save(commit=False)
+            pending_form.author = request.user
+            pending_form.save()
+            return redirect('stories:list_stories')
+    else:
+        form = StoryForm()
+        return render(request,
+                      'stories/add_story.html',
+                      {'form': form})
+
+
+@login_required
 def get_all_stories(request):
+    print('i am called')
     allstories = Story.objects.filter(author=request.user)
     return render(request, 'stories/list_stories.html',
                   {'allstories': allstories})
@@ -18,13 +36,13 @@ def story_detail(request, year, month, day, story):
                               publish__year=year,
                               publish__month=month,
                               publish__day=day)
-    comments = story.storycomments.filter(active=True)
+    comments = story.comments.filter(active=True)
     new_comment = None
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             new_comment = form.save(commit=False)
-            new_comment.story = story
+            new_comment.content_object = story
             new_comment.save()
     else:
         form = CommentForm()

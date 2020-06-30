039e97977cc0e80014568de58493426286b533db
Punit Jain <contactpunitjain@gmail.com>
2020-06-30 18:48:31 +0530

    added by Users view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import dashboard, get_all_artifacts, get_users_by_category, homepage
+from .views import dashboard, get_all_artifacts, get_users_by_category, homepage, byusers
 
 app_name = 'dashboard'
 
 urlpatterns = [
-    # path('', dashboard, name='front'),
     path('', homepage, name='front'),
     path('dashboard/', dashboard, name='dashboard'),
-    path('<int:id>/<str:user>/artifacts/',
-         get_all_artifacts,
-         name='get_all_artifacts'),
-    path('<int:catgy>/<str:ctryname>/',
-         get_users_by_category,
-         name='get_users_by_category')
+    path('dashboard/byuser', byusers, name='user_dashboard')
+    # path('<int:id>/<str:user>/artifacts/',
+    #      get_all_artifacts,
+    #      name='get_all_artifacts'),
+    # path('<int:catgy>/<str:ctryname>/',
+    #      get_users_by_category,
+    #      name='get_users_by_category')
 ]

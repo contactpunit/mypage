b1261e3d09c17827963fc309a9024a9a190e7ade
Punit Jain <contactpunitjain@gmail.com>
2020-07-01 13:31:45 +0530

    modified user dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import dashboard, get_all_artifacts, get_users_by_category, homepage, byusers
+from .views import dashboard, get_all_artifacts, \
+    get_users_by_category, homepage, byusers, artifact_details
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('', homepage, name='front'),
     path('dashboard/', dashboard, name='dashboard'),
-    path('dashboard/byuser', byusers, name='user_dashboard')
+    path('dashboard/byuser', byusers, name='user_dashboard'),
+    path('dashboard/artifacts', artifact_details, name='artifact_details'),
     # path('<int:id>/<str:user>/artifacts/',
     #      get_all_artifacts,
     #      name='get_all_artifacts'),

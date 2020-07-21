b3bba82a4cfcff81352081d0e98047d216894a1d
Punit Jain <contactpunitjain@gmail.com>
2020-07-21 19:14:16 +0530

    Added mysql support

--- dashboard/views.py
+++ dashboard/views.py
@@ -154,6 +154,7 @@ def upload_artifact(request):
         request.POST['slug'] = slug
         request.POST['status'] = 'published'
         if artifacttype == 'P':
+            print('P type')
             category = 'Photos'
             request.FILES['photo'] = artifact
         elif artifacttype == 'S':
